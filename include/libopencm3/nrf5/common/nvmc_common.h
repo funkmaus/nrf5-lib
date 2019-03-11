bb7b48d346ffc1686554aefabe2c786adfebc0d1
funkmaus <45281317+funkmaus@users.noreply.github.com>
2019-03-11 11:03:15 +0100

    Added UICR, Completed NVMC, Supplemented POWER

--- include/libopencm3/nrf5/common/nvmc_common.h
+++ include/libopencm3/nrf5/common/nvmc_common.h
@@ -1,6 +1,26 @@
 /** @addtogroup nvmc_defines
+ *
  * @author @htmlonly &copy; @endhtmlonly 2018
  * Gerrit Maus <funk@maus.xyz>, Luong Le <novercy@live.com>
+ *
+ */
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2018 Gerrit Maus <funk@maus.xyz>, Luong Le <novercy@live.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* THIS FILE SHOULD NOT BE INCLUDED DIRECTLY, BUT ONLY VIA NVMC.H
@@ -23,16 +43,52 @@ specific memorymap.h header before including this header file.*/
 /* --------------------- NVMC registers for NRF52 -------------------------- */
 
 /* Registers */
+#define NVMC_READY                        MMIO32((NVMC_BASE) + 0x400)
+#define NVMC_CONFIG                       MMIO32((NVMC_BASE) + 0x504)
+#define NVMC_ERASEPAGE                    MMIO32((NVMC_BASE) + 0x508)
+#define NVMC_ERASEALL                     MMIO32((NVMC_BASE) + 0x50C)
+#define NVMC_ERASEUICR                    MMIO32((NVMC_BASE) + 0x514)
 #define NVMC_ICACHECNF                    MMIO32((NVMC_BASE) + 0x540)
 /**@}*/
 
 /* --- NVMC_ICACHECNF values ----------------------------------------------------- */
 
+/** @defgroup nvmc_config NVMC configuration register
+@ingroup nvmc_defines
+@{*/
+#define NVMC_CONFIG_WEN_Ren            (0 << 0)
+#define NVMC_CONFIG_WEN_Wen            (1 << 0)
+#define NVMC_CONFIG_WEN_Een            (2 << 0)
+#define NVMC_CONFIG_WEN_REN            NVMC_CONFIG_WEN_Ren
+#define NVMC_CONFIG_WEN_WEN            NVMC_CONFIG_WEN_Wen
+#define NVMC_CONFIG_WEN_EEN            NVMC_CONFIG_WEN_Een
+/**@}*/
+
+/** @defgroup nvmc_eraseall NVMC ERASEALL register
+@ingroup nvmc_defines
+@{*/
+#define NVMC_ERASEALL_ERASEALL_Erase              (1 << 0)
+#define NVMC_ERASEALL_ERASEALL_ERASE              NVMC_ERASEALL_ERASEALL_Erase
+/**@}*/
+/** @defgroup nvmc_eraseuicr NVMC ERASEUICR register
+@ingroup nvmc_defines
+@{*/
+#define NVMC_ERASEUICR_ERASEUICR_Erase            (1 << 0)
+#define NVMC_ERASEUICR_ERASEUICR_ERASE            NVMC_ERASEUICR_ERASEUICR_Erase
+/**@}*/
+
 /** @defgroup nvmc_icachecnf I-Code cache configuration
 @ingroup nvmc_defines
 @{*/
-#define NVMC_ICACHECNF_CACHEEN_ENABLED            (1 << 0)
-/**@}*/
+#define NVMC_ICACHECNF_CACHEEN_Enabled            (1 << 0)
+#define NVMC_ICACHECNF_CACHEEN_ENABLED            NVMC_ICACHECNF_CACHEEN_Enabled
+#define NVMC_ICACHECNF_CHACHEPROFEN_Enabled       (1 << 8)
+#define NVMC_ICACHECNF_CHACHEPROFEN_ENABLED       NVMC_ICACHECNF_CHACHEPROFEN_Enabled
+#define NVMC_ICACHECNF_CACHEEN_Disabled           (0 << 0)
+#define NVMC_ICACHECNF_CACHEEN_DISABLED           NVMC_ICACHECNF_CACHEEN_Disabled
+#define NVMC_ICACHECNF_CHACHEPROFEN_Disabled      (0 << 8)
+#define NVMC_ICACHECNF_CHACHEPROFEN_DISABLED      NVMC_ICACHECNF_CHACHEPROFEN_Disabled
+/**@}*//* --- NVMC_ICACHECNF values ----------------------------------------------------- */
 
 #endif
 /** @cond */
@@ -40,3 +96,4 @@ specific memorymap.h header before including this header file.*/
 #warning "nvmc_common.h should not be included explicitly, only via nvmc.h"
 #endif
 /** @endcond */
+
--- include/libopencm3/nrf5/common/power_common.h
+++ include/libopencm3/nrf5/common/power_common.h
@@ -1,6 +1,26 @@
 /** @addtogroup power_defines
+ *
  * @author @htmlonly &copy; @endhtmlonly 2018
  * Gerrit Maus <funk@maus.xyz>, Luong Le <novercy@live.com>
+ *
+ */
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2018 Gerrit Maus <funk@maus.xyz>, Luong Le <novercy@live.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* THIS FILE SHOULD NOT BE INCLUDED DIRECTLY, BUT ONLY VIA POWER.H
@@ -24,13 +44,38 @@ specific memorymap.h header before including this header file.*/
 
 /* Registers */
 #define POWER_DCDCEN                       MMIO32((POWER_BASE) + 0x578)
+#define POWER_RESETREAS                    MMIO32((POWER_BASE) + 0x400)
+
 /**@}*/
+
+/* --- POWER_RESETREAS values ----------------------------------------------- */
+
+#define POWER_RESETREAS_RESETPIN_Detected          (1 << 0)
+#define POWER_RESETREAS_DOG_Detected               (1 << 1)
+#define POWER_RESETREAS_SREQ_Detected              (1 << 2)
+#define POWER_RESETREAS_LOCKUP_Detected            (1 << 3)
+#define POWER_RESETREAS_OFF_Detected               (1 << 16)
+#define POWER_RESETREAS_LPCOMP_Detected            (1 << 17)
+#define POWER_RESETREAS_DIF_Detected               (1 << 18)
+#define POWER_RESETREAS_NFC_Detected               (1 << 19)
+
+#define POWER_RESETREAS_RESETPIN_DETECTED   POWER_RESETREAS_RESETPIN_Detected
+#define POWER_RESETREAS_DOG_DETECTED        POWER_RESETREAS_DOG_Detected
+#define POWER_RESETREAS_SREQ_DETECTED       POWER_RESETREAS_SREQ_Detected
+#define POWER_RESETREAS_LOCKUP_DETECTED     POWER_RESETREAS_LOCKUP_Detected
+#define POWER_RESETREAS_OFF_DETECTED        POWER_RESETREAS_OFF_Detected
+#define POWER_RESETREAS_LPCOMP_DETECTED     POWER_RESETREAS_LPCOMP_Detected
+#define POWER_RESETREAS_DIF_DETECTED        POWER_RESETREAS_DIF_Detected
+#define POWER_RESETREAS_NFC_DETECTED        POWER_RESETREAS_NFC_Detected
 
 /* --- POWER_DCDCEN values -------------------------------------------------- */
 
 
-#define POWER_DCDCEN_DCDCEN_ENABLED                (1 << 0)
-#define POWER_DCDCEN_DCDCEN_DISABLED               (0 << 0)
+#define POWER_DCDCEN_DCDCEN_Enabled                (1 << 0)
+#define POWER_DCDCEN_DCDCEN_Disabled               (0 << 0)
+#define POWER_DCDCEN_DCDCEN_ENABLED                POWER_DCDCEN_DCDCEN_Enabled
+#define POWER_DCDCEN_DCDCEN_DISABLED               POWER_DCDCEN_DCDCEN_Disabled
+
 
 /* --- Function prototypes ------------------------------------------------- */
 
